8ace3340459223edcdece8d457aef2cb8aa217a1
Ayu Sudi <ayusudi.abc@gmail.com>
2025-01-06 22:54:04 +0700

    feat: chat role system assistant and user

--- pages/chat.py
+++ pages/chat.py
@@ -30,6 +30,9 @@ with col1:
     col1.info(
         f"Trigger our Posterio RAG as a PM Assistant by starting a chat with **/RAG** then continue with your prompt.\n\n**/RAG** I'm planning an inventory management system. What key skills should my developer have?"
     )
+
+
+if st.session_state.messages is None:
     st.session_state.messages = []
 
 # Detect model change and clear chat history if model has changed
@@ -62,14 +65,34 @@ def generate_chat_responses(chat_completion) -> Generator[str, None, None]:
             yield chunk.choices[0].delta.content
 
 
+text_system = f"""
+You are an AI Assistant for Project Managers, designed to streamline tech talent management through the application named Prosterio. You assist with tasks such as searching for candidates based on tech talent data uploaded by the user via the 'Add IT Talent' feature or viewing talent on the dashboard page.
+
+Purpose: Respond professionally and focus strictly on topics relevant to project management and tech talent.
+
+Guidelines:
+
+Relevance: Avoid providing detailed responses to unrelated topics, such as movies, personal questions, or casual chats. Politely redirect the user to ask questions within your domain expertise.
+
+Safety: Ensure responses are safe, respectful, and neutral, avoiding harm or controversial topics.
+
+Clarity: Provide clear, concise, and professional responses to maintain the credibility of the assistant.
+
+Examples:
+
+If asked about movies: "I'm here to assist with project management and tech talent tasks. For movie-related queries, I recommend consulting a dedicated platform."
+
+If asked a potentially harmful or controversial question: "I'm sorry, but I cannot assist with that query."
+
+This ensures that the assistant remains focused, safe, and aligned with its purpose."""
+
 def chatBot(max_tokens):
     try:
+        initialize = [{"role": "system", "content": text_system}]
+        messages = initialize + st.session_state.messages
         chat_completion = client.chat.completions.create(
             model=model_option,
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
+            messages=messages,
             max_tokens=max_tokens,
             stream=True,
         )
@@ -78,6 +101,8 @@ def chatBot(max_tokens):
         with st.chat_message("assistant", avatar="🤖"):
             chat_responses_generator = generate_chat_responses(chat_completion)
             full_response = st.write_stream(chat_responses_generator)
+
+        print(st.session_state.messages, "<<<<!")
     except Exception as e:
         st.error(e, icon="🚨")
 
@@ -94,35 +119,25 @@ def chatBot(max_tokens):
         )
 
 
-if len(st.session_state.messages) == 0:
-    prompt = st.chat_input("Start chat...")
-    if type(prompt) == str and prompt.lower().startswith("/rag"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user", avatar="👨‍💻"):
-            st.markdown(prompt)
-        # Fetch response from Groq API
-        try:
-            text = prompt.split("/rag")[0]
-            response = chatPromptRAG(text, st.session_state.user_info["email"])
-            st.session_state.messages.append(
-                {"assistant": "assistant", "content": response}
-            )
-            with st.chat_message("assistant", avatar="🤖"):
-                st.markdown(response[0][0])
-        except Exception as e:
-            st.error(e, icon="🚨")
-    elif type(prompt) == str and len(prompt) > 0:
-        print(prompt)
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user", avatar="👨‍💻"):
-            st.markdown(prompt)
-        chatBot(max_tokens=max_tokens)
-else:
-    if prompt := st.chat_input("Continue chat..."):
-        st.session_state.messages.append({"role": "user", "content": prompt})
+prompt = st.chat_input("Chat with PM Assistant")
 
-        with st.chat_message("user", avatar="👨‍💻"):
-            st.markdown(prompt)
-
-        # Fetch response from Groq API
-        chatBot(max_tokens=max_tokens)
+if type(prompt) == str and prompt.lower().startswith("/rag"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user", avatar="👨‍💻"):
+        st.markdown(prompt)
+    # Fetch response from Groq API
+    try:
+        text = prompt.split("/rag")[0]
+        response = chatPromptRAG(text, st.session_state.user_info["email"])
+        st.session_state.messages.append(
+            {"role": "assistant", "content": response[0][0]}
+        )
+        with st.chat_message("assistant", avatar="🤖"):
+            st.markdown(response[0][0])
+    except Exception as e:
+        st.error(e, icon="🚨")
+elif type(prompt) == str and len(prompt) > 0:
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user", avatar="👨‍💻"):
+        st.markdown(prompt)
+    chatBot(max_tokens=max_tokens)
